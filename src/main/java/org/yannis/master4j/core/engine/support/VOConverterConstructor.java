2f03bdaae77fb1e307a4febb87f302e00867362f
zhaoyjun0222@gmail.com <8560119zZ>
2016-09-11 20:14:54 +0800

    convert & controller

---
+++ src/main/java/org/yannis/master4j/core/engine/support/VOConverterConstructor.java
@@ -0,0 +1,57 @@
+package org.yannis.master4j.core.engine.support;
+
+import org.yannis.master4j.config.ProjectConfig;
+import org.yannis.master4j.entity.Field;
+import org.yannis.master4j.meta.ColumnMeta;
+import org.yannis.master4j.meta.TableMeta;
+import org.yannis.master4j.util.ClassUtils;
+import org.yannis.master4j.util.FieldUtils;
+import org.yannis.master4j.util.TemplateUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yannis on 16-9-11.
+ */
+public class VOConverterConstructor {
+    public static void construct(final String domainPath, final ProjectConfig projectConfig, final TableMeta meta) {
+        final String className = getClassName(meta);
+        final List<Field> fields = getFields(meta);
+        Map<String,Object> root = new HashMap<String,Object>(){
+            {
+                put("package", projectConfig.getBasePackageName()+".converter");
+                put("imports","");
+                put("classDoc",meta.getComment());
+                put("className", className);
+                put("fields",fields);
+            }
+        };
+
+        TemplateUtils.process("/springmvc/class/VoConverter.ftl", root, domainPath + "/" + className + ".java");
+    }
+
+    private static String getClassName(TableMeta meta) {
+        String tableName = meta.getTableName();
+        if (meta.getPrefixName() != null) {
+            tableName.replace(meta.getPrefixName(), "");
+        }
+
+        return ClassUtils.getCamelCaseName(tableName)+"VO";
+    }
+
+    private static List<Field> getFields(TableMeta meta) {
+        List<Field> fields = new ArrayList<>();
+        for(ColumnMeta columnMeta : meta.getColumnMetas()){
+            Field field = new Field();
+            field.setName(FieldUtils.getCamelCaseName(columnMeta.getColumnName()));
+            field.setType("int");
+            field.setSize(Integer.toString(columnMeta.getColumnSize()));
+            field.setComment(columnMeta.getComment());
+            fields.add(field);
+        }
+        return fields;
+    }
+}
